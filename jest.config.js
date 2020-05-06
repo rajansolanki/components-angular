6fdf9540dbb6ceb2498cf87c1994cbb7db4f1e70
Raj <rajansolanki@users.noreply.github.com>
2020-05-06 20:54:06 +0100

    ci: add config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/src/setup-jest.ts'],
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      { outputDirectory: '<rootDir>/reports/jest', outputName: 'jest.xml' },
+    ],
+  ],
+  coverageDirectory: '<rootDir>/reports/jest',
   globals: {
     'ts-jest': {
       diagnostics: {
