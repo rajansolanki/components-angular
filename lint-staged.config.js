ffbf042cef38e84f7848fce4691093cd8bc039ac
Raj <rajansolanki@users.noreply.github.com>
2020-05-06 13:43:23 +0100

    chore: add `tsc` commit hook

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  '*.ts': () => ['tsc --noEmit -p tsconfig.app.json', 'yarn lint'],
   '*.{ts,js,html,scss,md,yml,json}': (filenames) =>
     `prettier --write ${filenames.join(' ')}`,
 };
